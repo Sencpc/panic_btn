72f2e096904d6cf0b2bb7d13ecc0f3f98d188db9
Sen <seancornelius11@gmail.com>
2026-08-23 20:49:46 +0700

    more .env var and template env example

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const HMAC_SECRET_KEY = process.env.HMAC_SECRET_KEY;
 
